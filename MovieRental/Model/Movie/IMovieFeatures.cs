b372749eb10e403b20d7c32492ddaf0997bdcf8a
Yalle Prado <yalle.lbp@outlook.com>
2025-05-26 15:14:48 +0100

    Update .gitignore without .vsidx

--- MovieRental/Model/Movie/IMovieFeatures.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MovieRental.Movie;
-
-public interface IMovieFeatures
-{
-	Movie Save(Movie movie);
-	List<Movie> GetAll();
-	List<Movie> GetMovieByTitle(String title);
-}
--- MovieRental/Model/Payment/IPaymentFeatures.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MovieRental.Payment;
-
-public interface IPaymentFeatures
-{
-	Payment Save(Payment payment);
-
-	Payment Cancel (Payment payment, int id);
-}
