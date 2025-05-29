f2047bb6d39584f75e156b4ce800bc4752783a8b
Yalle Prado <yalle.prado@gmail.com>
2025-05-29 10:53:04 +0100

    feat: Update Customer Tables

---
+++ MovieRental/Migrations/20250529095144_UpdateTables.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieRental.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CustomerId",
+                table: "Rentals",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Payments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    RentalId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Customer = table.Column<string>(type: "TEXT", nullable: false),
+                    Provider = table.Column<string>(type: "TEXT", nullable: false),
+                    Price = table.Column<double>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Payments", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rentals_CustomerId",
+                table: "Rentals",
+                column: "CustomerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rentals_Customers_CustomerId",
+                table: "Rentals",
+                column: "CustomerId",
+                principalTable: "Customers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rentals_Customers_CustomerId",
+                table: "Rentals");
+
+            migrationBuilder.DropTable(
+                name: "Payments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Rentals_CustomerId",
+                table: "Rentals");
+
+            migrationBuilder.DropColumn(
+                name: "CustomerId",
+                table: "Rentals");
+        }
+    }
+}
